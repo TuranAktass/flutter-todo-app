94bb34d2f9500538183cbd1d51c3626cb108a876
TuranAktass <turan.ktas@gmail.com>
2021-10-24 21:50:46 +0300

    some ui changes and minor fixes

--- todo_app/lib/utils/color_constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color sapphire = Color(0xFF3450a1);
-const Color gulfBlue = Color(0xFF041955);
-const Color anakiwa = Color(0xFF97B4FF);
-const Color electricViolet = Color(0xFFEB06FF);
